864ef198c38aac0a1f733bb6c6de24f75c02e0cc
Dawin Camilo Cortes <dawin@mctekk.com>
2021-01-10 12:45:43 -0400

    Remove unncesary main.js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
-import { rtdbPlugin } from "vuefire";
-createApp(App).use(rtdbPlugin).mount("#app");
-//# sourceMappingURL=main.js.map
